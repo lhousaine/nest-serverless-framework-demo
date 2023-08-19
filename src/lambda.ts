841fd4cad65fa9643f96a2d88558c113dd22ee39
lhouarhou <l.ouarhou.ensa@gmail.com>
2023-08-19 12:28:25 +0100

    feat(lambda): configure project lambda function entry point

---
+++ src/lambda.ts
@@ -0,0 +1,36 @@
+// lambda.ts
+import { Handler, Context } from 'aws-lambda';
+import { Server } from 'http';
+import { createServer, proxy } from 'aws-serverless-express';
+import { eventContext } from 'aws-serverless-express/middleware';
+
+import { NestFactory } from '@nestjs/core';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import { AppModule } from './app.module';
+
+const express = require('express');
+
+const binaryMimeTypes: string[] = [];
+
+let cachedServer: Server;
+
+// Create the Nest.js server and convert it into an Express.js server
+async function bootstrapServer(): Promise<Server> {
+  if (!cachedServer) {
+    const expressApp = express();
+    const nestApp = await NestFactory.create(
+      AppModule,
+      new ExpressAdapter(expressApp),
+    );
+    nestApp.use(eventContext());
+    await nestApp.init();
+    cachedServer = createServer(expressApp, undefined, binaryMimeTypes);
+  }
+  return cachedServer;
+}
+
+// Export the handler : the entry point of the Lambda function
+export const handler: Handler = async (event: any, context: Context) => {
+  cachedServer = await bootstrapServer();
+  return proxy(cachedServer, event, context, 'PROMISE').promise;
+};
